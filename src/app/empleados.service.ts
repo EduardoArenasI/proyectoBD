01df5929e490541b141db190093fb18b82853f30
EduardoArenasI <eduardoarenas014@gmail.com>
2022-11-11 11:17:20 -0600

    update service

--- src/app/empleados.service.ts
+++ src/app/empleados.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore} from '@angular/fire/compat/firestore';
+import { empleado} from './empleado.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadosService {
 
-  constructor() { }
+  constructor(private angularFirestore: AngularFirestore) { 
+    
+  }
 }
